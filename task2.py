64f81694f32eabe2ad0fd348820362f94f4d34bd
Sachin Kumar Verma <skvbca32@gmail.com>
2024-09-04 01:04:44 +0530

    first commit

---
+++ task2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+data = pd.read_csv("Unemployment.csv")
+print(data.head())
